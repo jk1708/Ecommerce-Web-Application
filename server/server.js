6699f3ddfad8add139de5b99ba4ebece3432d048
Jaishree Khatri <jaishreekhatri17@gmail.com>
2019-06-12 15:31:21 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -26,13 +26,7 @@ require('../config/passport');
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 app.use(cookieParser());
-var jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-
-var $ = jQuery = require('jquery')(window);
+var $= require('jQuery');
 app.use(session({
    secret: 'mysupersecret',
    resave: false,
